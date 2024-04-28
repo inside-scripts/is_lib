a09bc07a2d7466dcf445bccf38a12ccdf7f2ce5d
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-04-28 19:36:17 +0200

    Update main.lua

--- is_lib/server/main.lua
+++ is_lib/server/main.lua
@@ -13,7 +13,7 @@ Lib.checkVersion = function()
 
 	local currentVersion = GetResourceMetadata(resource, 'version')
 
-		PerformHttpRequest("https://api.github.com/repos/inside-scripts/is_lib/releases/latest", function(status, response)
+	PerformHttpRequest("https://api.github.com/repos/inside-scripts/is_lib/releases/latest", function(status, response)
             if status ~= 200 then return end
 
             local data = json.decode(response)
@@ -21,7 +21,7 @@ Lib.checkVersion = function()
             if currentVersion ~= data.tag_name then
                 print("^2[UPDATE] ^7An update for ^2is_lib ^7is available. Download the latest version from ^2GitHub^7: ^5https://github.com/inside-scripts/is_lib/releases/tag/"..data.tag_name)
             end
-		end, 'GET')
+	end, 'GET')
 
 end
 
