ab0e45e51856443a9c6a1c2058306e02c8a880c1
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-05-01 18:15:31 +0200

    Update main.lua

--- is_lib/server/main.lua
+++ is_lib/server/main.lua
@@ -20,6 +20,11 @@ Lib.checkVersion = function()
 
             if currentVersion ~= data.tag_name then
                 print("^2[UPDATE] ^7An update for ^2is_lib ^7is available. Download the latest version from ^2GitHub^7: ^5https://github.com/inside-scripts/is_lib/releases/tag/"..data.tag_name)
+            else
+                print("^2[INFORMATION] ^7Resource ^2is_lib ("..currentVersion..") ^7has been started successfully.")
+                print("^7Store: ^5https://inside-scripts.tebex.io")
+                print("^7Documentation: ^5https://inside-scripts.gitbook.io/documentation")
+                print("^7Discord: ^5https://discord.gg/URKNMSwx5W")
             end
 	end, 'GET')
 
