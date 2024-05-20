239f45138053a55eb3988fc4e370a3c2f5547f27
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-05-20 10:44:51 +0200

    Update framework.lua

--- is_lib/client/framework.lua
+++ is_lib/client/framework.lua
@@ -1,35 +1,49 @@
-Core = nil
+if cfg.Framework then
+    CreateThread(function()
+        if cfg.Framework == "QBCore" then
+            while GetResourceState("qb-core") ~= "started" do
+                Wait(5)
+            end
 
-if cfg.Framework == "QBCore" then
-    Core = exports["qb-core"]:GetCoreObject()
-  
-    RegisterNetEvent('QBCore:Client:OnJobUpdate', function(job)
-        TriggerEvent('is_lib:Client:OnJobUpdate', {name = job.name})
+            Wait(1000)
+
+            Core = exports["qb-core"]:GetCoreObject()
+        
+            RegisterNetEvent('QBCore:Client:OnJobUpdate', function(job)
+                TriggerEvent('is_lib:Client:OnJobUpdate', {name = job.name})
+            end)
+        elseif cfg.Framework == "ESX" then
+            while GetResourceState("es_extended") ~= "started" do
+                Wait(5)
+            end
+
+            Wait(1000)
+
+            Core = exports["es_extended"]:getSharedObject()
+
+            RegisterNetEvent('esx:setJob', function(job) 
+                TriggerEvent('is_lib:Client:OnJobUpdate', {name = job.name})
+            end)
+        end
+
+        while not Core do
+            Wait(5)
+        end
+
+        Lib.getPlayerData = function()
+            local PlayerData
+
+            if cfg.Framework == "QBCore" then
+                PlayerData = Core.Functions.GetPlayerData()
+            elseif cfg.Framework == "ESX" then
+                PlayerData = Core.GetPlayerData()
+            end
+
+            while not PlayerData do
+                Wait(5)
+            end
+
+            return PlayerData
+        end
     end)
-elseif cfg.Framework == "ESX" then
-    Core = exports["es_extended"]:getSharedObject()
-
-    RegisterNetEvent('esx:setJob', function(job) 
-        TriggerEvent('is_lib:Client:OnJobUpdate', {name = job.name})
-    end)
-end
-
-while not Core do
-    Wait(5)
-end
-
-Lib.getPlayerData = function()
-    local PlayerData
-
-    if cfg.Framework == "QBCore" then
-        PlayerData = Core.Functions.GetPlayerData()
-    elseif cfg.Framework == "ESX" then
-        PlayerData = Core.GetPlayerData()
-    end
-
-    while not PlayerData do
-        Wait(5)
-    end
-
-    return PlayerData
 end
