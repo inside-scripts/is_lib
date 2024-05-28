6da9cd81dbb0033f5a5e360fa80c19d8d648aeed
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-05-28 16:51:28 +0200

    Update fxmanifest.lua

--- is_lib/fxmanifest.lua
+++ is_lib/fxmanifest.lua
@@ -8,7 +8,7 @@ lua54 'yes'
 
 name 'is_lib'
 author 'inside-scripts'
-version '1.0.4'
+version '1.0.5'
 repository 'https://github.com/inside-scripts/is-lib'
 description 'A Library that facilitates the use of multiple frameworks, inventories and various systems in one. It also includes many features that enhance functionality.'
 
@@ -17,22 +17,24 @@ description 'A Library that facilitates the use of multiple frameworks, inventor
 client_scripts {
     'config.lua',
     'client/main.lua',
+    'shared/functions.lua',
     'client/framework.lua',
     'client/callbacks.lua',
     'client/fuel.lua',
     'client/vehiclekeys.lua',
     'client/entity.lua',
-    'shared/functions.lua',
+    'client/blip.lua',
 }
 
 server_scripts {
     'config.lua',
     'server/main.lua',
+    'shared/functions.lua',
     'server/framework.lua',
     'server/timezone.lua',
     'server/webhook.lua',
     'server/commands.lua',
     'server/callbacks.lua',
     'server/inventory.lua',
-    'shared/functions.lua',
+    'server/entity.lua',
 }
