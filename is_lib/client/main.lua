24b54e3c00db6e1e75644a0235c72d1e1eece837
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-06-03 12:33:20 +0200

    Update main.lua

--- is_lib/client/main.lua
+++ is_lib/client/main.lua
@@ -1,9 +1,44 @@
 Lib = {}
+cache = {}
+blips = {}
 
 exports('GetLibObject', function()
+    local calledBy = GetInvokingResource()
+
     while cfg.Framework and not Core do
         Wait(5)
     end
 
+    cache[calledBy] = {}
+    blips[calledBy] = {}
+
     return Lib
+end)
+
+AddEventHandler("onClientResourceStop", function(resource)
+    if cache[resource] then
+        local deleteEntity = false
+
+        for entity, _ in pairs(cache[resource]) do
+            Lib.deleteEntity(entity, resource)
+            deleteEntity = true
+        end
+
+        if deleteEntity then
+            print(("[%s] has been stopped, removing objects."):format(resource))
+        end
+    end
+
+    if blips[resource] then
+        local deleteBlip = false
+        
+        for blip, _ in pairs(blips[resource]) do
+            Lib.removeBlip(blip, resource)
+            deleteBlip = true
+        end
+
+        if deleteBlip then
+            print(("[%s] has been stopped, removing blips."):format(resource))
+        end
+    end
 end)
