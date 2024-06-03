e923bad47763ba0842f53c1636e14d805a8e49b0
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-06-03 12:23:33 +0200

    Update blip.lua

--- is_lib/client/blip.lua
+++ is_lib/client/blip.lua
@@ -1,4 +1,6 @@
 Lib.createBlipCoords = function(coords, args)
+    local calledBy = GetInvokingResource()
+
     local sprite = args.sprite or 1
     local display = args.display or 2
     local scale = args.scale or 0.6
@@ -18,18 +20,38 @@ Lib.createBlipCoords = function(coords, args)
     AddTextComponentString(name)
     EndTextCommandSetBlipName(blip)
 
+    if calledBy and blips[calledBy] then
+        blips[calledBy][blip] = true
+    end
+
     return blip
 end
 
 Lib.createBlipRadius = function(coords, args)
+    local calledBy = GetInvokingResource()
+
+    local radius = args.radius or 50.0
     local color = args.color or 1
     local alpha = args.alpha or 255
     local shortRange = args.shortRange or false
 
-    local blip = AddBlipForRadius(coords.x, coords.y, coords.z, args.radius)
+    local blip = AddBlipForRadius(coords.x, coords.y, coords.z, radius)
     SetBlipColour(blip, color)
     SetBlipAlpha(blip, alpha)
     SetBlipAsShortRange(blip, shortRange)
 
+    if calledBy and blips[calledBy] then
+        blips[calledBy][blip] = true
+    end
+
     return blip
+end
+
+Lib.removeBlip = function(blip, resource)
+    local calledBy = resource or GetInvokingResource()
+
+    if blips[calledBy] and blips[calledBy][blip] then
+        RemoveBlip(blip)
+        blips[calledBy][blip] = nil
+    end
 end
