b35e1d17d867157e2d547b598d3034df17fdc74e
inside-scripts <168145041+inside-scripts@users.noreply.github.com>
2024-06-24 15:16:05 +0200

    Update config.lua

--- is_lib/config.lua
+++ is_lib/config.lua
@@ -5,7 +5,8 @@ cfg.Time                 = {                  -- The time is retrieved directly 
     ["Format"] = 12                           -- 12, 24
 }
 
-cfg.Framework            = "QBCore"           -- "QBCore" or "ESX"
+cfg.Framework            = "QBCore"           -- "QBCore", "ESX", nil
+cfg.Interaction          = "is_interaction"   -- "is_interaction", "qb-target", "ox_target"
 cfg.Inventory            = "qb-inventory"     -- "qb-inventory", "ox_inventory", "qs-inventory", "ps-inventory", "lj-inventory", "origen_inventory", "codem-inventory", "ESX"
 cfg.Fuel                 = "LegacyFuel"       -- "LegacyFuel", "cdn-fuel", "ps-fuel", "okokGasStation", "ox_fuel", "lj-fuel", "hyon_gas_station", "ND_Fuel", "myFuel"
 cfg.VehicleKeys          = "qb-vehiclekeys"   -- "qb-vehiclekeys", "qs-vehiclekeys", "vehicles_keys", "mk_vehiclekeys", "wasabi_carlock", "cd_garage", "okokGarage"
